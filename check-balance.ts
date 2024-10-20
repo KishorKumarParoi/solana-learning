a09a0f68eb471eb60cd7f8301237da4e5a89a202
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-10-21 00:36:36 +0600

    Handle invalid wallet address

--- check-balance.ts
+++ check-balance.ts
@@ -5,17 +5,31 @@ import {
   PublicKey,
 } from "@solana/web3.js";
 
-const connection = new Connection(clusterApiUrl("devnet"));
-const publicKey = new PublicKey("JDjsHewXMA68XcEc5QD7xJpazusS9WEcDUaap3ZjWs49");
-const balance = await connection.getBalance(publicKey);
-console.log(`The balance of the account is: `, balance / LAMPORTS_PER_SOL);
+async function main() {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"));
+    const publicKey = new PublicKey(
+      "JDjsHewXMA68XcEc5QD7xJpazusS9WEcDUaap3ZjWs49"
+    );
+    const balance = await connection.getBalance(publicKey);
+    console.log(`The balance of the account is: `, balance / LAMPORTS_PER_SOL);
 
-const suppliedKey = process.argv[2];
-const suppliedKeyPair = new PublicKey(suppliedKey);
-const suppliedBalance = await connection.getBalance(suppliedKeyPair);
-console.log(
-  `The balance of the supplied account is: `,
-  suppliedBalance / LAMPORTS_PER_SOL
-);
+    const suppliedKey = process.argv[2];
+    if (!PublicKey.isOnCurve(suppliedKey)) {
+      throw new Error("Invalid public key supplied.");
+    }
 
-console.log(`✅ Finished! We've checked the balance of the account!`);
+    const suppliedKeyPair = new PublicKey(suppliedKey);
+    const suppliedBalance = await connection.getBalance(suppliedKeyPair);
+    console.log(
+      `The balance of the supplied account is: `,
+      suppliedBalance / LAMPORTS_PER_SOL
+    );
+
+    console.log(`✅ Finished! We've checked the balance of the account!`);
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+main();
